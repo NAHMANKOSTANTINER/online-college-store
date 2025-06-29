55c6f563e398af47f73997b84be94fcfeee2cefe
nahman <4784114@gmail.com>
2025-06-29 17:41:15 +0300

    ivori

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-alert("Welcome to College Store.");
+const { default: chalk } = require('chalk');
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/',(request,response)=>{
+    response.write('Ivore Homepage!')
+    response.end();
+
+});
+app.get('/prodacts',(request,response)=>{
+    response.write('Ivore prodacts!')
+    response.end();
+
+});
+
+app.listen(PORT ,function(){
+    console.log(chalk.blue('server is running'))
+});
